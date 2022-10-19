ae819e90fd197690a934a420cff888a42a82c1ad
karpana <ptrudel@trudel-networks.com>
2022-10-18 21:18:23 -0500

    updates for DLAPI formatting

--- Source/Modules/ReputationBarsCommon.lua
+++ Source/Modules/ReputationBarsCommon.lua
@@ -1,26 +1,27 @@
 ReputationBarsCommon = {}
+local appName = "ReputationBars"
 
 local isFormatComplete = false
 local isLoggingEnabled = true -- this should be false whenever possible
 
 function ReputationBarsCommon:DebugLog (type,category,level,message)
   if isLoggingEnabled then
-    local debugmsg = ""
+    local debugMsg = ""
     if type ~= nil and type ~= "" then
-      debugmsg = debugmsg .. type .. "~"
+      debugMsg = debugMsg .. type .. "~"
     end
     
     if category ~= nil and category ~= "" then
-      debugmsg = debugmsg .. category .. "~"
+      debugMsg = debugMsg .. category .. "~"
     end
 
     if level ~= nil and level ~= "" then
-      debugmsg = debugmsg .. level .. "~"
+      debugMsg = debugMsg .. level .. "~"
     end
 
-    debugmsg = debugmsg .. message
+    debugMsg = debugMsg .. message
     
-    if DLAPI then DLAPI.DebugLog("ReputationBars", debugmsg) end
+    if DLAPI then DLAPI.DebugLog(appName, debugMsg) end
 
     if isFormatComplete == false then
       ReputationBarsCommon:FormatDebugLog()
@@ -30,9 +31,9 @@ end
 
 function ReputationBarsCommon:FormatDebugLog()
   if DLAPI and DLAPI.GetFormat and DLAPI.IsFormatRegistered then
-    local fmt = DLAPI.IsFormatRegistered(DLAPI.GetFormat(addonName))
+    local fmt = DLAPI.IsFormatRegistered(DLAPI.GetFormat(appName))
     if fmt and fmt.colWidth then
-      fmt.colWidth = { 0.05, 0.12, 0.11, 0.03, 1 - 0.05 - 0.12 - 0.11 - 0.03, }
+      fmt.colWidth = { 0.05, 0.12, 0.20, 0.03, 1 - 0.05 - 0.12 - 0.20 - 0.03, }
     end
   end
   isFormatComplete = true
